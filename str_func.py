45dc6700cb33b9143e65be183f4e8c6703d10bcb
Alexander38work <malov-av1128@yandex.ru>
2024-04-20 13:44:24 -0400

    str function change and d_str

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def str_func(text):
+    """все заглавные"""
     str_text = text.upper()
     return str_text
+
+def str_first_symbol (text):
+    """первые заглавные"""
+    str_first = text.title()
+    return str_first
